ecdc98bc11dd9762d300366104e31be065f219cc
Ranu4922 <s.ranushyan@gmail.com>
2026-06-08 15:52:59 +0200

    Refine settlement growth display

--- scripts/settlement_growth_info.gd
+++ scripts/settlement_growth_info.gd
@@ -13,27 +13,32 @@ func _process(_delta: float) -> void:
 
 func _update_growth_info() -> void:
 	var growth_active: bool = bool(population_growth.call("get_growth_active"))
-	var growth_text: String = "aktiv" if growth_active else "blockiert"
+	var status_text: String = "Aktiv" if growth_active else "Blockiert"
 	var required_food: int = int(population_growth.call("get_required_food"))
 	var current_food: int = int(hex_grid.get("food"))
 	var free_housing: int = int(hex_grid.get("free_housing"))
-	var next_resident_text: String = "-"
-	var block_reason: String = "-"
-
-	if growth_active:
-		var remaining_seconds: int = int(population_growth.call("get_remaining_seconds"))
-		next_resident_text = "%d Sekunden" % remaining_seconds
-	else:
-		block_reason = String(population_growth.call("get_block_reason"))
-
 	var lines: PackedStringArray = PackedStringArray([
 		"",
 		"Wachstum",
-		"Wachstum: %s" % growth_text,
+		"Status: %s" % status_text,
 		"Benötigte Nahrung: %d" % required_food,
 		"Aktuelle Nahrung: %d" % current_food,
 		"Freier Wohnraum: %d" % free_housing,
-		"Nächster Bewohner in: %s" % next_resident_text,
-		"Blockiert wegen: %s" % block_reason,
 	])
+
+	if growth_active:
+		var remaining_seconds: int = int(population_growth.call("get_remaining_seconds"))
+		lines.append("Nächster Bewohner in: %d Sekunden" % remaining_seconds)
+	else:
+		lines.append("Grund: %s" % _get_display_block_reason())
+
 	text = "\n".join(lines)
+
+
+func _get_display_block_reason() -> String:
+	var block_reason: String = String(population_growth.call("get_block_reason"))
+	if block_reason == "zu wenig Nahrung":
+		return "Zu wenig Nahrung"
+	if block_reason == "kein freier Wohnraum":
+		return "Kein freier Wohnraum"
+	return block_reason
